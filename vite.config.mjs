c80fb2db075777a7ca09878429439a8545d04f40
Art <artwitwit@gmail.com>
2024-11-02 20:55:48 +0300

    Change vite entry point

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
     plugins: [
         meteor({
-          clientEntry: 'client/main.js',
+          clientEntry: 'imports/ui/main.js',
           // stubValidation: {
           //   warnOnly: true,
           // },
