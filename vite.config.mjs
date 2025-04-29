613131f8fa40431fe93129714693bec358f4975f
Art <artwitwit@gmail.com>
2025-04-29 13:40:23 +0300

    ⚡️ Add bundle visualizer

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,19 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import { meteor } from 'meteor-vite/plugin';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-    plugins: [
-        meteor({
-          clientEntry: 'imports/ui/main.js',
-          stubValidation: {
-            warnOnly: true,
-          },
-          enableExperimentalFeatures: true, // Required to enable server bundling.
-        }),
-        svelte({
-          configFile: 'svelte.config.mjs'
-          /* plugin options */
-        })
-    ],
-})
+  plugins: [
+    meteor({
+      clientEntry: 'imports/ui/main.js',
+      stubValidation: {
+        warnOnly: true,
+      },
+      enableExperimentalFeatures: true, // Required to enable server bundling.
+    }),
+    svelte({
+      configFile: 'svelte.config.mjs',
+      /* plugin options */
+    }),
+    /**
+     * meteor npm run build
+     * cd output/svelte/bundle/programs/server && npm install
+     * For more details see here: ../output/svelte/bundle/programs/web.browser/app/vite
+     */
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: true,
+      template: 'sunburst',
+    }),
+  ],
+});
